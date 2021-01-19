3fac95187429f3946f1de1f091e7657a35382661
Smrati-S <smratindu20@gmail.com>
2021-01-19 16:06:20 +0530

    Update Required_Functions.py
    Removed unnecessary comments

--- Server/Required_Functions.py
+++ Server/Required_Functions.py
@@ -274,28 +274,3 @@ def output(img, sol, av,arr):
     # mask=cv2.bitwise_not(mask)
     #cv2.imshow("bg", bg)
     return bg, mask
-
-
-'''
-def solver(puzzle):
-    for itrations in range(0, 3):
-        zones = extract_zones(puzzle)
-        for zone in zones:
-            if zone["type"] == "row":
-                row = zone["coord"]
-                for col in range(0, 9):
-                    insert_possibilities(puzzle, row, col)
-            elif zone["type"] == "col":
-                col = zone["coord"]
-                for row in range(0, 9):
-                    insert_possibilities(puzzle, row, col)
-            else:
-                row_begin = zone["coord"][0]
-                row_end = zone["coord"][1]
-                col_begin = zone["coord"][2]
-                col_end = zone["coord"][3]
-                for row in range(row_begin, row_end + 1):
-                    for col in range(col_begin, col_end + 1):
-                        insert_possibilities(puzzle, row, col)
-    return puzzle
-'''
